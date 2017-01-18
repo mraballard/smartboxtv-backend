dd6b74fe03b3fd9996f0ab9e213ad46a6f273451
Aaron Ballard <mraballard@gmail.com>
2017-01-18 18:48:08 -0300

    Refactored code, added comments, removed unnecessary packages

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,11 +1,23 @@
+///////////////////////////////////////////////////
+// Purpose: Defines all routes supported by server
+///////////////////////////////////////////////////
+
 var express     = require('express');
 var router      = express.Router();
 var Team        = require('../models/team');
 var Player      = require('../models/player');
+var server      = require('../server.js');
 
+// Root route redirects to api/teams route
+// Params: None
+router.get('/', function(req, res) {
+  res.redirect('/api/teams');
+});
 
+// Return list of all teams
+// Params: None
 router.get('/api/teams', function(req, res) {
-  Team.find().exec()
+  Team.find()
   .then(function(teams){
     res.json(teams);
   })
@@ -13,16 +25,25 @@ router.get('/api/teams', function(req, res) {
     console.log(err);
   })
 });
+
+// Return Team and associated players
+// Params:
+//   idTeam: Team ID (String)
 router.get('/api/teams/:idTeam', function(req, res) {
-  Team.find({_id: req.params.idTeam}).exec()
+  // For simplicity, no validation checking for :idTeam
+  Team.find({_id: req.params.idTeam})
   .then(function(team){
     res.json(team);
   })
   .catch(function(err) {
     console.log(err);
+    res.json({status: 500, message: "An error occurred"});
   })
 });
 
+// Return only list of players on team
+// Params:
+//   idTeam: Team ID (String)
 router.get('/api/teams/:idTeam/players', function(req, res) {
   Player.find({team: req.params.idTeam}).exec()
   .then(function(players){
@@ -30,10 +51,14 @@ router.get('/api/teams/:idTeam/players', function(req, res) {
   })
   .catch(function(err) {
     console.log(err);
+    res.json({status: 500, message: "An error occurred"});
   })
 });
 
-router.get('/api/teams/:idTeam/players/:position', function(req, res) {
+// Return players at requested position on active teams
+// Params:
+//   position: Player Position (String)
+router.get('/api/teams/players/:position', function(req, res) {
   Team.find({active: true}).exec()  // Find all teams that are active
   .then(function(teams){
     var activePlayers = [];
@@ -54,6 +79,7 @@ router.get('/api/teams/:idTeam/players/:position', function(req, res) {
   })
   .catch(function(err) {
     console.log(err);
+    res.json({status: 500, message: "An error occurred"});
   })
 });
 
--- server.js
+++ server.js
@@ -1,99 +1,100 @@
-var express       = require('express');
-var bodyParser    = require('body-parser');
-var parseString   = require('xml2js').parseString;
-var convert       = require('xml-js');
-var mongoose      = require('mongoose');
-var logger        = require('morgan');
-var http          = require('http');
-var pry           = require('pryjs');
-var app           = express();
-var router        = express.Router();
-var indexController = require('./controllers/indexController.js');
-var Team        = require('./models/team');
-var Player      = require('./models/player');
+///////////////////////////////////////////////////
+// Purpose: Requires necessary packages and runs
+// server on port 3000
+///////////////////////////////////////////////////
 
-// app.use(bodyParser.json());  // Need this?
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(xmlParser());
-app.use(logger('dev'));
+var express         = require('express');
+var parseString     = require('xml2js').parseString;
+var mongoose        = require('mongoose');
+var http            = require('http');
+var app             = express();
+var indexController = require('./controllers/indexController.js');
+
+// Import Mongoose Schemas
+var Team            = require('./models/team');
+var Player          = require('./models/player');
+
+// http port that we will be listening on
+var port = 3000;
+
 app.use('/', indexController);
 
-
+// Initialize Mongoose connection
 mongoose.Promise = global.Promise;
 var mongoURI = 'mongodb://localhost/smartboxtv-backend';
 mongoose.connect(mongoURI);
 
-
-//Databse Setup
+//Databse Setup and start server after database check
 var db = mongoose.connection;
 db.on('error', function(err) { console.log(err); });
-db.once('open', function(){ console.log('Connected to Mongo database'); });
-
-
-app.get('/', function(req, res) {
-  res.json({status: 200, message: "Server up and running"});
-
-  db.collection('teams').count(function (err, count) {
-    // CHECK IF DATABASE IS EMPTY
-      if (!err && count === 0) {
-        var myObj = {};
-        // IF EMPTY, POPULATE DATABASE USING XML AT FOLLOWING URL
-        var options = {
-              hostname: "s3.amazonaws.com",
-              path: '/nunchee-fxassets-local/dump.xml'
-          };
-        // GET ROUTE FOR DATABASE INITIALIZATION - XML DATA SOURCE
-        http.get(options, function (response) {
-            var completeResponse = '';
-            response.on('data', function (chunk) {
-              // Gather XML as string
-                completeResponse += chunk;
-            });
-            response.on('end', function() {
-              // Parse XML to JSON
-              parseString(completeResponse, function (err, result) {
-                // Create Teams from JSON data
-                result.sport.team.forEach(function(teamObj) {
-                  Team.create({
-                    _id: teamObj.$.id,
-                    name: teamObj.$.name,
-                    active: teamObj.$.active,
-                    players: []
-                  })
-                  .then(function(team){
-                    // If team has players, create players and add them to Player array on Team object
-                    if (teamObj.player) {
-                      teamObj.player.forEach(function(playerObj) {
-                        console.log("PLAYER OBJECT 1 " + playerObj.$.id);
-                        Player.create({
-                          _id: playerObj.$.id,
-                          name: playerObj.name,
-                          position: playerObj.position,
-                          team: team.id
-                        })
-                        .then(function(player){
-                          console.log("Player :" +  player);
-                          team.players.push(player);
-                          team.save();
-                        })
-                        .catch(function(err) {
-                          console.log(err);
-                        })
-                      });
-                    }
-                    return team;
-                  })
-                  .catch(function(err){
-                    console.log(err);
-                  });
-                });
-              });
-            });  // Response end
-          }).on('error', function (e) {
-            console.log('problem with request: ' + e.message);
-        });
-      }
+db.once('open', function() {
+  initializeDatabase();
+  app.listen(port, function(){
+    console.log('=======================');
+    console.log('Running on port ' + port);
+    console.log('========================');
   });
 });
 
-app.listen(3000);
+// Initialize database if no fixture data exists
+// Params: None
+function initializeDatabase() {
+
+  db.collection('teams').count(function (err, count) {
+    if (!err && count === 0) {
+      var myObj = {};
+      var options = {
+        hostname: "s3.amazonaws.com",
+          path: '/nunchee-fxassets-local/dump.xml'
+      };
+      http.get(options, function (response) {
+        var completeResponse = '';
+        response.on('data', function (chunk) {
+          // Gather XML as string
+          completeResponse += chunk;
+        });
+        response.on('end', function() {
+          // Parse XML to JSON
+          parseString(completeResponse, function (err, result) {
+            // Create Teams from JSON data
+            result.sport.team.forEach(function(teamObj) {
+              Team.create({
+                _id: teamObj.$.id,
+                name: teamObj.$.name,
+                active: teamObj.$.active,
+                players: []
+              })
+              .then(function(team){
+                // If team has players, create players and add them to Player array on Team object
+                if (teamObj.player) {
+                  teamObj.player.forEach(function(playerObj) {
+                    Player.create({
+                      _id: playerObj.$.id,
+                      name: playerObj.name,
+                      position: playerObj.position,
+                      team: team.id
+                    })
+                    .then(function(player){
+                      team.players.push(player);
+                      team.save();
+                    })
+                    .catch(function(err) {
+                      console.log(err);
+                    })
+                  });
+                }
+                return team;
+              })
+              .catch(function(err){
+                console.log(err);
+              });
+            });
+          });
+        });  // Close response.on finish
+      }) // Close http request
+      .on('error', function (e) {
+          console.log('problem with request: ' + e.message);
+      });
+    }
+  });
+}
