b788e9f244c4fb7d716e90bdc67f025e5b519471
Aaron Ballard <mraballard@gmail.com>
2017-01-18 12:09:19 -0300

    Successfully parsing XML to JSON, looping through Team array of objects, storing teams in database. Need to add players to teams

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var express       = require('express');
-var app           = express();
 var bodyParser    = require('body-parser');
-var xmlParser     = require('express-xml-bodyparser');
+var parseString   = require('xml2js').parseString;
+var convert       = require('xml-js');
 var mongoose      = require('mongoose');
+var logger        = require('morgan');
+var http          = require('http');
+var pry           = require('pryjs');
+var app           = express();
 var router        = express.Router();
 var indexController = require('./controllers/indexController.js');
 var Team        = require('./models/team');
@@ -10,7 +14,8 @@ var Player      = require('./models/player');
 
 // app.use(bodyParser.json());  // Need this?
 // app.use(bodyParser.urlencoded({extended: false}));
-app.use(xmlParser());
+// app.use(xmlParser());
+app.use(logger('dev'));
 app.use('/', indexController);
 
 
@@ -24,23 +29,40 @@ var db = mongoose.connection;
 db.on('error', function(err) { console.log(err); });
 db.once('open', function(){ console.log('Connected to Mongo database'); });
 
-// GET ROUTE FOR DATABASE INITIALIZATION - XML DATA SOURCE
-db.collection('teams').count(function (err, count) {
-  // CHECK IF DATABASE IS EMPTY
-    if (!err && count === 0) {
-      // IF EMPTY, POPULATE DATABASE
-      Team.create({name: 'Test', active: false})
-        .then(function(res) {
-          console.log(db.collection('teams').find());
-        });
-    }
-});
-
-
-
 
 app.get('/', function(req, res) {
   res.json({status: 200, message: "Server up and running"});
+
+  db.collection('teams').count(function (err, count) {
+    // CHECK IF DATABASE IS EMPTY
+      if (!err && count === 0) {
+        var myObj = {};
+        // IF EMPTY, POPULATE DATABASE
+        var options = {
+              hostname: "s3.amazonaws.com",
+              path: '/nunchee-fxassets-local/dump.xml'
+          };
+        // GET ROUTE FOR DATABASE INITIALIZATION - XML DATA SOURCE
+        http.get(options, function (response) {
+            var completeResponse = '';
+            response.on('data', function (chunk) {
+                completeResponse += chunk;
+            });
+            response.on('end', function() {
+              // var result = convert.xml2json(completeResponse, {compact: true, spaces:4, fullTagEmptyElement: true});
+              // console.log(result);
+              parseString(completeResponse, function (err, result) {
+                // eval(pry.it);
+                result.sport.team.forEach(function(teamObj) {
+                  Team.create(teamObj.$);
+                });
+              });
+            });
+          }).on('error', function (e) {
+            console.log('problem with request: ' + e.message);
+        });
+      }
+  });
 });
 
 app.listen(3000);
